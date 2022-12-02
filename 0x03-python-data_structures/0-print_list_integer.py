96354c5e79abcf10604c4ee419e4570d1ab1956a
IsaacMbonye <mbonyeisac@gmail.com>
2022-12-02 10:11:43 +0000

    Print a list of integers

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,9 @@
+def print_list_integer(my_list=[]):
+    my_list=[]
+    for i in range(3):
+       a = input('Enter a number: ')
+       my_list.append(a)
+    print("{}\n{}\n{}\n".format(my_list[0],my_list[1],my_list[2]))
+
+
+    
