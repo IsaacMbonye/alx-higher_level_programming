cb6b6c2d0b5776ce562ffea986cd8041c43c387f
IsaacMbonye <mbonyeisac@gmail.com>
2022-12-28 20:24:18 +0000

    islower.py

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -2,10 +2,8 @@
 #The ASCII value of the lowercase alphabet is from 97 to 122. And, the ASCII value of the uppercase alphabet is from 65 to 90.
 #Write a program that prints the ASCII alphabet, in lowercase, not followed by a new line.
 
-import string
-
-for letter in string.ascii_lowercase:
-    print(string.ascii_lowercase.index(letter) + 97, end = ' ')
+for letter in 'abcdefghijklmnopqrstuvwxyz':
+    print("{}".format(letter), end = '')
 
 #print(ascii_alphabet, end ='')
 
